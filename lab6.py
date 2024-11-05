6691ed83b690542325957eeff479da0d37418c12
groom <cgroom@calpoly.edu>
2024-11-04 23:03:45 -0600

    Final Work

--- lab6.py
+++ lab6.py
@@ -35,14 +35,59 @@ def selection_sort(values:list[int]) -> None:
         values[mindex] = values[idx]
         values[idx] = tmp
 
-
 # Part 1
+def smallest_index(books: list[data.Book], start: int) -> Optional[int]: # the argument books is expected to be a list
+    # of instances of the class Book, while the argument start is expected to be an integer, and the function has an
+    # optional output of integer
+    if start >= len(books) or start < 0: # if there is 1 or fewer books, don't change anything
+        return None
 
+    mindex = start # assign mindex the value of start
+    for idx in range(start + 1, len(books)): # compares the title of the current book to the next book
+        if books[idx].title < books[mindex].title: # if the current book is alphabetically smaller than the next, set it
+            #as the new mindex
+            mindex = idx
+    return mindex # returns the alphabetically smallest book from the given starting point
+
+
+def alphabetical_sort(books: list[data.Book]) -> None: # argument books is expected to be a list of instances of the
+    # class Books, and the function will not output anything because it is modifying the given list
+    for idx in range(len(books) - 1): # repeats for total books in the list, except for the last
+        mindex = smallest_index(books, idx) # calls the function smallest_index() to get the mindex
+        books[mindex], books[idx] = books[idx], books[mindex] # swaps the current book with the alphabetically smallest book
 
 # Part 2
+def swap_case(letters: str) -> str: # the argument letters is expected to be a string, and the function will output a string
+    final = [] # creates a list called final
+    for i in letters: # repeats for every character in the string
+        if i.isupper(): # if the character is uppercase, then turn it into lowercase and add it to the final list
+            temp = i.lower()
+            final.append(temp)
+        elif i.islower(): # if the character is lowercase, then turn it into uppercase and add it to the final list
+            temp = i.upper()
+            final.append(temp)
+        else: # otherwise add it to the final list
+            final.append(i)
+    return "".join(final) # returns the final list with all characters joined to create a string
 
 
 # Part 3
-
+def str_translate(letters: str, old: str, new: str) -> str: # the argument letters is expected to be a (long) string,
+    # while the arguments old and new are expected to be (one character) strings
+    letters_list = list(letters) # turns the argument letters from a string into a list of characters
+    for i in range(len(letters)): # repeats for the total characters in letters_list
+        if letters_list[i] == old: # replaces the old character with the new character
+            letters_list[i] = new
+    return "".join(letters_list) # returns letter_list with all characters joined to create a string with the new character
 
 # Part 4
+def histogram_letters(sentence: str) -> dict: # the argument sentence is expected to be a string, and the function is
+    # expected to output a dictionary
+    count = {} # creates an empty dictionary
+    lst = [x.strip(",") for x in sentence.split(" ")] # creates a list of all words in a sentence and removes commas from the words
+    for word in lst: # repeats for every word in lst
+        if word not in count: # if the word isn't already in the dictionary, add it with the value 1
+            count[word] = 1
+        else: # otherwise add 1 to the value of the word in the dictionary count
+            count[word] += 1
+    return count #returns the dictionary count
--- lab6_tests.py
+++ lab6_tests.py
@@ -1,6 +1,7 @@
 import data
 import lab6
 import unittest
+from lab6 import swap_case, str_translate, histogram_letters
 
 
 # Write your test cases for each part below.
@@ -64,19 +65,68 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
+    def test_alphabetical_sort1(self):
+        books = [data.Book(["Henry"],"Harry Potter"),
+                 data.Book(["Larry"], "The Wild Things"),
+                 data.Book(["Kieran"], "How to Live")]
+        lab6.alphabetical_sort(books)
+        expected = [data.Book(['Henry'], 'Harry Potter'),
+                    data.Book(['Kieran'], 'How to Live'),
+                    data.Book(['Larry'], 'The Wild Things')]
+        self.assertEqual(books, expected)
 
+    def test_alphabetical_sort2(self):
+        books = [data.Book(["Homer"],"The Iliad"),
+                 data.Book(["Homer"], "The Odyssey"),
+                 data.Book(["Sophocles"], "Oedipus")]
+        lab6.alphabetical_sort(books)
+        expected = [data.Book(["Sophocles"], "Oedipus"),
+                    data.Book(["Homer"],"The Iliad"),
+                    data.Book(["Homer"], "The Odyssey")]
+        self.assertEqual(books, expected)
 
     # Part 2
+    def test_swap_case1(self):
+        list = "Hello! Welcome to Cal Poly."
+        result = swap_case(list)
+        expected = "hELLO! wELCOME TO cAL pOLY."
+        self.assertEqual(result, expected)
 
+    def test_swap_case2(self):
+        list = "Goodbye! Come back in 3 weeks."
+        result = swap_case(list)
+        expected = "gOODBYE! cOME BACK IN 3 WEEKS."
+        self.assertEqual(result, expected)
 
     # Part 3
+    def test_str_translate1(self):
+        string = 'abcdcba'
+        old = 'a'
+        new = 'x'
+        result = str_translate(string, old, new)
+        expected = 'xbcdcbx'
+        self.assertEqual(result, expected)
 
+    def test_str_translate2(self):
+        string = 'Hello Lauren'
+        old = 'l'
+        new = '1'
+        result = str_translate(string, old, new)
+        expected = 'He11o Lauren'
+        self.assertEqual(result, expected)
 
     # Part 4
+    def test_histogram_letters1(self):
+        sentence = "This is a long sentence, but is it a very long sentence"
+        result = histogram_letters(sentence)
+        expected = {'This': 1, 'a': 2, 'but': 1, 'is': 2, 'it': 1, 'long': 2, 'sentence': 2, 'very': 1}
+        self.assertEqual(result, expected)
 
-
-
-
+    def test_histogram_letters2(self):
+        sentence = "I am Cole, I am not Jon, and I wrote this code, not Jon"
+        result = histogram_letters(sentence)
+        expected = {'Cole': 1, 'I': 3, 'Jon': 2, 'am': 2, 'and': 1, 'code': 1, 'not': 2, 'this': 1, 'wrote': 1}
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
